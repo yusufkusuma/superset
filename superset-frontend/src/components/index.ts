ee24a172780f30ae4183bdce10edbc1575c654df
Evan Rusackas <evan@rusackas.com>
2024-07-10 09:57:53 -0600

    cleaning....

--- superset-frontend/src/components/index.ts
+++ superset-frontend/src/components/index.ts
@@ -24,6 +24,7 @@
  */
 export { default as Select } from './Select/Select';
 export { default as AsyncSelect } from './Select/AsyncSelect';
+export { default as Card } from './Card';
 
 /*
  * Components that don't conflict with the ones in src/components.
@@ -55,8 +56,7 @@ export {
  * or extending the components in src/components.
  */
 export {
-  Breadcrumb as AntdBreadcrumb,
-  Button as AntdButton,
+  Breadcrumb as AntdBreadcrumb, // TODO: Make this a real Component
   Card as AntdCard,
   Checkbox as AntdCheckbox,
   Collapse as AntdCollapse,
